319fe2509e99cdfb3a75b38bb98c2f58f30663c0
nyanyani <dobx@live.com>
2021-08-25 11:39:33 +0800

    Init with docusaurus

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,99 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github")
+const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "laboUI",
+  tagline: "React 自定义组件库",
+  url: "https://nyanyani.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "nyanyani", // Usually your GitHub org/user name.
+  projectName: "laboUI", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "laboUI",
+      logo: {
+        alt: "laboUI Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/nyanyani/laboUI",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "About",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/nyanyani",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/nyanyani/laboUI",
+            },
+          ],
+        },
+      ],
+      copyright: `MIT licensed | Copyright © ${new Date().getFullYear()} Nyanyani, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+}
